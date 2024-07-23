ffe4aa52b2df0e56669f505105998d7c8be85de9
unknown <m.giri1309@gmail.com>
2024-07-23 09:20:56 +0700

    fix autoconplete

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -73,6 +73,7 @@ const RegisterPage = () => {
                 name="username"
                 type="text"
                 required
+                autoComplete="username"
                 className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm shadow-md"
                 placeholder="Username"
                 value={username}
