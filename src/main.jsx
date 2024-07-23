6c6778adc0cb5ebc40d6b5d69be21054dd0c9c8e
unknown <m.giri1309@gmail.com>
2024-07-23 09:02:48 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NowPlaying from "./pages/NowPlayingPage";
+import PopularPage from "./pages/PopularPage";
+import ErrorPage from "./pages/ErrorPage";
+import DetailsPage from "./pages/DetailsPage";
+import ProfilePage from "./pages/ProfilePage";
+
+import "animate.css";
+import LoginPage from "./pages/LoginPage";
+import UpComingPage from "./pages/UpcomingPage";
+import RegisterPage from "./pages/RegisterPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/now-playing",
+    element: <NowPlaying />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/popular",
+    element: <PopularPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/upcoming",
+    element: <UpComingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/details/:id",
+    element: <DetailsPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
